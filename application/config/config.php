6d1a5ee4dbb373126ba00a9bbcabcd65deb98c4a
Camilo Velasquez <Camilo Velasquez>
2016-12-17 15:04:38 -0500

    Cambio base_url()

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sepp/';
+$config['base_url'] = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/sepp0/';
 
 /*
 |--------------------------------------------------------------------------
