c5df141fcb2d281b1218bce5605bc3e009b00285
Camilo Velasquez <Camilo Velasquez>
2016-12-17 12:22:38 -0500

    Se quita index.php de la url para interfaz amigable

--- application/config/config.php
+++ application/config/config.php
@@ -36,7 +36,7 @@ $config['base_url'] = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
